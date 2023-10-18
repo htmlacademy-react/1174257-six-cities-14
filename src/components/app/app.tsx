7c6ccb337b70e278134b06cfa97954df1c8d8e12
Aleksandr Medvedev <a.medvedev@liga-team.ru>
2023-10-18 23:37:10 +0300

    fix: добавляет другие страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,21 @@
-import {MainProps, Main} from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Main, {MainProps} from '../../pages/main/main';
+import NotFound from '../../pages/not-found/not-found';
 
 type AppProps = {
   main: MainProps;
 }
 
-export function App(props: AppProps): JSX.Element {
+export default function App(props: AppProps): JSX.Element {
+  // временно будет так и без типов
   return (
     props.main && <Main {...props.main} />
+    ||
+    props.login && <Login />
+    ||
+    props.notFound && <NotFound />
+    ||
+    props.favorites && <Favorites {...props.favorites} />
   );
 }
