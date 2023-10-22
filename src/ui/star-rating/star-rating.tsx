58a10fe3f831726ec0d8010632132f60536a2ebd
Aleksandr Medvedev <a.medvedev@liga-team.ru>
2023-10-22 20:00:30 +0300

    fix: использет .map в звездном рейтинге

--- src/ui/star-rating/star-rating.tsx
+++ src/ui/star-rating/star-rating.tsx
@@ -2,94 +2,37 @@ import classNames from 'classnames';
 
 type StarRatingProps = {
   className: string;
+  starsCount?: number;
 }
 export default function StarRating({
   className,
+  starsCount = 5,
 }: StarRatingProps): JSX.Element {
   const starClass = classNames('form__rating', className);
+  const elements = Array(starsCount).fill('');
 
   return (
     <div className={starClass}>
-      <input
-        className="form__rating-input visually-hidden"
-        name="rating"
-        defaultValue={5}
-        id="5-stars"
-        type="radio"
-      />
-      <label
-        htmlFor="5-stars"
-        className="form__rating-label"
-        title="perfect"
-      >
-        <svg className="form__star-image" width={37} height={33}>
-          <use xlinkHref="#icon-star" />
-        </svg>
-      </label>
-      <input
-        className="form__rating-input visually-hidden"
-        name="rating"
-        defaultValue={4}
-        id="4-stars"
-        type="radio"
-      />
-      <label
-        htmlFor="4-stars"
-        className="form__rating-label"
-        title="good"
-      >
-        <svg className="form__star-image" width={37} height={33}>
-          <use xlinkHref="#icon-star" />
-        </svg>
-      </label>
-      <input
-        className="form__rating-input visually-hidden"
-        name="rating"
-        defaultValue={3}
-        id="3-stars"
-        type="radio"
-      />
-      <label
-        htmlFor="3-stars"
-        className="form__rating-label"
-        title="not bad"
-      >
-        <svg className="form__star-image" width={37} height={33}>
-          <use xlinkHref="#icon-star" />
-        </svg>
-      </label>
-      <input
-        className="form__rating-input visually-hidden"
-        name="rating"
-        defaultValue={2}
-        id="2-stars"
-        type="radio"
-      />
-      <label
-        htmlFor="2-stars"
-        className="form__rating-label"
-        title="badly"
-      >
-        <svg className="form__star-image" width={37} height={33}>
-          <use xlinkHref="#icon-star" />
-        </svg>
-      </label>
-      <input
-        className="form__rating-input visually-hidden"
-        name="rating"
-        defaultValue={1}
-        id="1-star"
-        type="radio"
-      />
-      <label
-        htmlFor="1-star"
-        className="form__rating-label"
-        title="terribly"
-      >
-        <svg className="form__star-image" width={37} height={33}>
-          <use xlinkHref="#icon-star" />
-        </svg>
-      </label>
+      {elements.map((item, i, array) => (
+        <>
+          <input
+            className="form__rating-input visually-hidden"
+            name="rating"
+            defaultValue={array.length - i}
+            id={`${array.length - i}-stars`}
+            type="radio"
+          />
+          <label
+            htmlFor={`${array.length - i}-stars`}
+            className="form__rating-label"
+            title="perfect"
+          >
+            <svg className="form__star-image" width={37} height={33}>
+              <use xlinkHref="#icon-star" />
+            </svg>
+          </label>
+        </>
+      ))}
     </div>
   );
 }
