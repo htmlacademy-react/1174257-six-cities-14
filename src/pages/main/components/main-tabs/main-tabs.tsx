d9312d75cc21666676674a45bf1580c955f05a7f
Aleksandr Medvedev <a.medvedev@liga-team.ru>
2023-10-30 21:21:43 +0300

    fix: рефакторит список ссылок городов

--- src/pages/main/components/main-tabs/main-tabs.tsx
+++ src/pages/main/components/main-tabs/main-tabs.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import uniqid from 'uniqid';
 
@@ -12,31 +13,21 @@ export type MainTabsProps = {
 };
 
 export default function MainTabs({list}: MainTabsProps): JSX.Element {
+  const linkClass = (isActive: boolean) => classNames('locations__item-link tabs__item', {['tabs__item--active']: isActive});
+
   return (
     <div className="tabs">
       <section className="locations container">
         <ul className="locations__list tabs__list">
           {list.map((item) => (
             <li className="locations__item" key={uniqid()}>
-
-              {item.isActive && (
-                <Link
-                  className='locations__item-link tabs__item tabs__item--active'
-                  to={item.href}
-                >
-                  <span>{item.text}</span>
-                </Link>
-              )}
-
-              {!item.isActive && (
-                <Link
-                  className='locations__item-link tabs__item'
-                  to={item.href}
-                >
-                  <span>{item.text}</span>
-                </Link>
-              )}
-
+              <Link
+                className={linkClass(Boolean(item.isActive))}
+                to={item.href}
+                aria-current={item.isActive}
+              >
+                <span>{item.text}</span>
+              </Link>
             </li>
           ))}
         </ul>
